6a7d6474f03a7eac0d32cac31796fd5150d3267e
Dipankar1kumar <dipankar.k.s.v.m@gmail.com>
2024-05-13 22:49:31 +0530

    initial setup

---
+++ about.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+  
+
+<!-- End Google Tag Manager -->
+    <title>About - Maa Vaishno Jewellers</title>
+        <!-- <link rel="shortcut icon" href="assets/logo.png" style="background-color:#666"> -->
+        <?php
+    include 'head.php';
+    ?>
+</head>
+<body>
+    <div class="container-fluid">
+    <?php
+    include 'header.php';
+    ?>
+
+        <section class="page-heading">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">About</li>
+                </ol>
+            </nav>
+            <h4>About us</h4>
+        </section>
+
+        <section class="who-we-are container about">
+            <div class="row heading">
+                <div class="container text-center">
+                    <h4>Maa Vaishno Jewellers who?</h4>
+                    <p>Maa Vaishno Jewellers offers beautiful, handcrafted jewelry and silver items influenced by tradition. We have stunning rings, earrings, bracelets, and necklaces, as well as lovely home decor and intricately designed mandirs. Our specialty lies in crafting antique jewelry, making us a top choice for both timeless and contemporary designs.</p>
+                </div>
+            </div>
+            <div class="row images">
+                <div class="col-md-4"><img src="assets/maavaishnojewellers1.jpg" alt=""></div>
+                <div class="col-md-4"><img src="assets/maavaishnojewellers2.jpg" alt=""></div>
+                <div class="col-md-4"><img src="assets/maavaishnojewellers3.jpg" alt=""></div>
+            </div>
+            <div class="row content justify-content-md-center">
+                <div class="col-md-8">
+                    <div class="container text-center">
+                        <h3>History of Maa Vaishno Jewellers and Customer Satisfaction.</h3>
+                        <p>
+                        At Maa Vaishno Jewellers, we make beautiful jewelry, from gold bangles to bridal sets and gold rings, all with a touch of elegance. Our collection includes silver and gold pendants, each with its own special design and high quality. we've been a trusted retailer, offering unique pieces at prices you can afford.
+
+                    
+                        </p>
+                        <p>
+                        In Bettiah, our talented designers mix modern styles with new ideas. For four generations, we've gained trust and loyalty. Making customers happy is our top priority. Our designs celebrate important moments in life and we promise to keep delivering great quality for all our customers, old and new.
+                                    
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="seperator"></div>
+        
+        <section class="passion about">
+            <div class="container">
+                <div class="row heading">
+                    <div class="container text-center">
+                        <h4>Vision & Mission</h4>
+                    </div>
+                </div>
+                <div class="row content">
+                    <div class="col passion-col">
+                        <h4>Vision</h4>
+                        <p>
+                        At Maa Vaishno Jewellers, we make your dreams come true with beautiful, handcrafted jewelry that's perfectly designed. You can pick something special from our ready-made collection or personalize your own piece with our special customization services.<br><br></p>
+                    </div>
+                    <div class="col style-col">
+                        <h4>Mission</h4>
+                        <p>
+                        At Maa Vaishno Jewellers, we're all about making sure you're happy, and we do it by being honest and fair in everything we do. We promise to keep upholding these values as we help you find the perfect piece of jewelry.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php
+    include 'footer.php';
+    include 'foot.php';
+    ?>
+    </div>
+</body>
+
+</html>
