ac33f6ee9b4a061b730f7d14a97c4d51a94bf09f
Sugandh Kumar <sugandhkumar9@gmail.com>
2024-08-03 22:12:24 +0530

    chore: Update favicon link in head.php

--- head.php
+++ head.php
@@ -2,4 +2,5 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/index.css">
      <!-- <link rel="shortcut icon" href="favicon.ico" style="background-color:#666"> -->
+    <link rel="shortcut icon" href="assets/logo.png">
   <meta name="keywords" content=" Discover exquisite gold, silver, bronze, and antique jewelry at Maa Vaishno Jewellers, Bihar's premier jewel store. Explore a stunning collection of fine jewels including diamond pendants, 98k gold bracelets, elegant chains, earrings, marriage and wedding rings, perfect for gifting or special occasions. Benefit from our gold savings scheme with low wastage and best prices. Shop now for timeless elegance and impeccable craftsmanship. ">
